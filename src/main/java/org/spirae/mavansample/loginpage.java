464eda0c5259d05d7a556ccd3090d2b4111ccdcd
swathiB <bheemireddyswathi2@gmail.com>
2024-08-14 11:20:11 -0700

    fourth commit-removes annotation in java class

--- src/main/java/org/spirae/mavansample/loginpage.java
+++ src/main/java/org/spirae/mavansample/loginpage.java
@@ -2,12 +2,10 @@ package org.spirae.mavansample;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class loginpage extends loginpageobjects {
 	WebDriver  driver;
